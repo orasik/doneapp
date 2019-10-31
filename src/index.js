e2fd6efe1c5faf4ec16154159dbe737fe5b44a12
Oras Al-Kubaisi <hi@oras.me>
2019-10-31 14:55:03 +0000

    added caporal lib, refactored into commands

--- src/index.js
+++ src/index.js
@@ -2,23 +2,46 @@
 const inquirer = require('inquirer');
 const { addTask, listTasks }  = require('./store');
 
-const questions = [
-  {
-    type: 'input',
-    name: 'reference',
-    message: 'What is the ticket number?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: "Please describe the work.",
-  }
-];
+const prog = require('caporal');
+prog
+  .version('1.0.0')
+  // you specify arguments using .argument()
+  // 'app' is required, 'env' is optional
+  .command('add', 'Add a new task')
+  // .argument('<app>', 'App to deploy', /^myapp|their-app$/)
+  .argument('[date]', 'Date where task was done', /^dev|staging|production|local$/, 'local')
+  // you specify options using .option()
+  // if --tail is passed, its value is required
+  // .option('--tail <lines>', 'Tail <lines> lines of logs after deploy', prog.INT)
+  .action(function(args, options, logger) {
+    console.log(args);
+    console.log(options);
+    const questions = [
+      {
+        type: 'input',
+        name: 'reference',
+        message: 'What is the ticket number?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: "Please describe the work.",
+      }
+    ];
+    
+    inquirer.prompt(questions).then(answers => {
+      answers['timestamp'] = new Date().getTime()
+      answers['category'] = 'work'
+    
+      addTask(answers);
+    });
+  })
+  
+  .command('list', 'List tasks done today')
+  .argument('[date]', 'List tasks done on that date')
+  .action((args, options, logger) => {
+    listTasks(args['date']);
+  })
+  ;
 
-inquirer.prompt(questions).then(answers => {
-  answers['timestamp'] = new Date().getTime()
-  answers['category'] = 'work'
-
-  addTask(answers);
-  listTasks('2019-10-27');
-});
+prog.parse(process.argv);
