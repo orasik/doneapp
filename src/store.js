afd49694f9dfb152a998c63614bd13cab400244b
Oras Al-Kubaisi <hi@oras.me>
2019-11-02 20:52:52 +0000

    code style for store

--- src/store.js
+++ src/store.js
@@ -1,43 +1,42 @@
-'use strict';
 
-const {databaseURL, serviceAccount, collection} = require ('./config');
+const admin = require('firebase-admin');
+const uuidv4 = require('uuid/v4');
+const moment = require('moment');
+const { databaseURL, serviceAccount, collection } = require('./config');
 
-const admin = require ('firebase-admin');
-const uuidv4 = require ('uuid/v4');
-const moment = require ('moment');
-admin.initializeApp ({
-  credential: admin.credential.cert (serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
   databaseURL,
 });
 
-let db = admin.firestore ();
+const db = admin.firestore();
 
-const addTask = async function (note, date = null) {
+const addTask = async (note, date = null) => {
   if (date == null) {
-    note['timestamp'] = moment ().unix ();
+    note.timestamp = moment().unix();
   } else {
-    note['timestamp'] = moment (date).unix ();
+    note.timestamp = moment(date).unix();
   }
-  note['category'] = 'work';
-  const document = db.collection (collection).doc (uuidv4 ());
+  note.category = 'work';
+  const document = db.collection(collection).doc(uuidv4());
   try {
-    await document.set (note);
-    console.log ('added new data into the document');
+    await document.set(note);
+    console.log('added new data into the document');
   } catch (e) {
-    console.error (e);
+    console.error(e);
   }
 };
-const listTasks = async function (date) {
-  const startOfDay = moment (date).startOf ('day').unix ();
-  const endOfDay = moment (date).endOf ('day').unix ();
+const listTasks = async (date) => {
+  const startOfDay = moment(date).startOf('day').unix();
+  const endOfDay = moment(date).endOf('day').unix();
   const todayTasks = db
-    .collection ('tasks')
-    .where ('timestamp', '<=', endOfDay)
-    .where ('timestamp', '>=', startOfDay);
+    .collection('tasks')
+    .where('timestamp', '<=', endOfDay)
+    .where('timestamp', '>=', startOfDay);
   try {
     return await todayTasks.get();
   } catch (e) {
-    console.error (e);
+    console.error(e);
   }
 };
 
